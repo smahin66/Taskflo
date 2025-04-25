2541ce2cddf4c283c6d6cf382f9ef86165610f4f
smahin66 <83550756+smahin66@users.noreply.github.com>
2025-04-25 13:34:06 +0200

    Update main.tsx

--- project/src/main.tsx
+++ project/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import { LanguageProvider } from './contexts/LanguageContext';
 import './index.css';
+import './exposeSupabase'; // 👈 Ajoute cette ligne ici
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
